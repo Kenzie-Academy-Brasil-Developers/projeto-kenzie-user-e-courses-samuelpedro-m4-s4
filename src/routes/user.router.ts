ea6122067484b41be16be65f13fc7a52145154ae
Samuel <samuelpedro0001@gmail.com>
2023-10-19 19:03:38 -0300

    entrega concluida

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -3,8 +3,6 @@ import { validateBody } from "../middlewares/validateBody.middleware";
 import { userCreateSchema } from "../schemas/user.schema";
 import { createUserController, getUserCourseController, readUserController } from "../controllers/user.controller";
 import { uniqueEmail } from "../middlewares/uniqueEmail.middlewares";
-import { loginSchema } from "../schemas/login.schema";
-import { loginController } from "../controllers/login.controller";
 import { verifyToken } from "../middlewares/verifyToken.middleware";
 import { verifyPermission } from "../middlewares/verifyPermission.middleware";
 import { checkUserCoursesMiddleware } from "../middlewares/checkCurse.middleware";
@@ -12,8 +10,6 @@ import { checkUserCoursesMiddleware } from "../middlewares/checkCurse.middleware
 export const userRouter: Router = Router();
 
 userRouter.post("/", validateBody(userCreateSchema), uniqueEmail, createUserController);
-
-userRouter.post("/login", validateBody(loginSchema), loginController);
 
 userRouter.get("/", verifyToken, verifyPermission, readUserController);
 
